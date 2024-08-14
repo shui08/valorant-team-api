050a7464848cc89f269e85174278028d1b339ac3
Anand Venkatesan <anandv0854@gmail.com>
2024-08-14 03:42:21 -0700

    app.go explanation

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,3 +1,6 @@
+// this code aims to create a connection to a MySQL database as well as
+// a db (database) variable that can be used throughout the program to refer
+// to the database connection (use GetDB to access it).
 package config
 
 import (
@@ -5,9 +8,19 @@ import (
 	"gorm.io/gorm"
 )
 
+// declaring db, which will be used to refer to the database connection
 var db *gorm.DB
 var placeholder string
 
+// this function is what creates a connection to the database, and we will use
+// gorm to do so. the reason for using gorm is so that we can interact with the
+// database using Go, rather than SQL. mysql.Open() takes in a database
+// connection string and returns a MySQL driver instance, which gorm can use
+// to connect with the database. gorm.Open() takes in that driver instance as
+// well as a config struct, and actually creates the database connection, which
+// is stored in `data` as a *gorm.DB instance. it also returns an error, and if
+// that error is non-nil, we will call panic. we then set db, the package-wide
+// connection instance variable, equal to data.
 func Connect() {
 	data, err := gorm.Open(mysql.Open(placeholder), &gorm.Config{})
 	if err != nil {
@@ -16,6 +29,7 @@ func Connect() {
 	db = data
 }
 
+// this function is simply for accessing the db variable outside of the package.
 func GetDB() *gorm.DB {
 	return db
 }
